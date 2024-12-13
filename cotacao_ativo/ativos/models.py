146bef349be75a32cba54327635d5c51c90bb558
LukasFalk25 <lucasdilima00@gmail.com>
2024-12-13 03:53:24 -0300

    feat: foi criado o model Ativo e registrado no admin

--- cotacao_ativo/ativos/models.py
+++ cotacao_ativo/ativos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Ativo(models.Model):
+    nome = models.CharField(max_length=120)
+    codigo = models.CharField(max_length=14, unique=True)
+    limite_inferior = models.FloatField()
+    limite_superior = models.FloatField()
+    periodicidade = models.IntegerField(help_text="Periodicidade em minutos")
+    
+def __str__(self):
+    return f"Ativo: {self.nome}, Código: {self.codigo}"
